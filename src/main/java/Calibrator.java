7739da26bb9f24a937489a85b62227362cfb66df
darivs <dtack192@gmail.com>
2018-12-10 09:06:43 +0100

    Update list of freqs to hashset for much better performance

--- src/main/java/Calibrator.java
+++ src/main/java/Calibrator.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Calibrator {
     private int[] result = new int[2];
@@ -9,21 +10,18 @@ class Calibrator {
     }
 
     private int[] calculateFrequency(List<String> frequencyChanges) {
-        List<Integer> frequenciesToCompare = new ArrayList<>();
+        Set<Integer> frequenciesToCompare = new HashSet<>();
 
         while (result[1] == 0) {
-            for (int i = 0; i < frequencyChanges.size(); i++) {
-                int number = Integer.parseInt(frequencyChanges.get(i).substring(1));
+            for (int i = 0; i < frequencyChanges.size(); i++) { // foreach
+                int number = Integer.parseInt(frequencyChanges.get(i));
 
-                if (frequencyChanges.get(i).charAt(0) == '+') {
-                    result[0] += number;
-                } else {
-                    result[0] -= number;
-                }
+                result[0] += number;
 
                 if (frequenciesToCompare.contains(result[0])) {
                     result[1] = result[0];
                     i = frequencyChanges.size();
+                    //break;
                 }
 
                 frequenciesToCompare.add(result[0]);
